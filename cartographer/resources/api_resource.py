2936f5711c28928ee09f3c38628eb921aaf8a3ec
David Kettler <21echoes@gmail.com>
2016-04-17 17:06:02 -0700

    much more sane approach to resource registry via the APIResource class

--- cartographer/resources/api_resource.py
+++ cartographer/resources/api_resource.py
@@ -1,3 +1,29 @@
+from cartographer.parsers.schema_parser import SchemaParser
+from cartographer.permissions.base_mask import BaseMask
+from cartographer.resources import get_resource_registry_container
+from cartographer.schemas.schema import Schema
+from cartographer.serializers import SchemaSerializer
+
+
 class APIResource(object):
-    # TODO: register with the resource_registry
-    pass
+    SCHEMA = Schema
+    SERIALIZER = SchemaSerializer
+    PARSER = SchemaParser
+    MASK = BaseMask
+    MODEL = None
+
+    @classmethod
+    def type_string(cls):
+        return cls.SCHEMA.schema()['type']
+
+    @classmethod
+    def register(cls):
+        registry = get_resource_registry_container()
+        registry.register_resource(
+            type_string=cls.type_string(),
+            schema=cls.SCHEMA,
+            model=cls.MODEL,
+            serializer=cls.SERIALIZER,
+            parser=cls.PARSER,
+            mask=cls.MASK
+        )
